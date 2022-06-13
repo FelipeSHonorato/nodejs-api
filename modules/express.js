288edf0cd16d7c62eeb2abc09dbe9d74749ae76a
Felipe <felipeshonorato@gmail.com>
2022-06-13 14:05:32 -0300

    fix: Removido arquivos de front do projeto.

--- modules/express.js
+++ modules/express.js
@@ -3,7 +3,6 @@ const UserModel = require("../src/models/user.model");
 
 const app = express();
 
-app.set("views", "src/views");
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(`Request Type:${req.method}`);
