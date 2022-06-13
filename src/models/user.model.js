d9d9685f0310b59542b0c0127d45cf049f7a9f2a
Felipe <felipeshonorato@gmail.com>
2022-06-13 20:08:05 -0300

    refactor: Modificados atributos em user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -16,6 +16,12 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+    deafult: "https://icon-library.com/icon/anonymous-avatar-icon-25.html",
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    deafult: false,
   },
   password: {
     type: String,
